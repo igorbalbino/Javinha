f1e252cd910a0620b355a752918cee2210f5f1e8
Igor do Espírito Santo Balbino <igorbalbinox1@hotmail.com>
2024-05-19 13:44:17 -0300

    adicionando codigo funcional quicksort. consegui fazer sem olhar

--- QuickSort.java
+++ QuickSort.java
@@ -1,59 +1,47 @@
+/*
+https://youtu.be/BW2qE_7HNG8?si=M5GmWBqhNO2JxGzl
+*/
 class QuickSort {
 	public static void main(String[] args) {
-		int [] arr = {5, 2, 7, 4, 9, 8, 6};
+		int[] arr = {9,1,5,4,6,8,11,45,22,37,32,42,88,91,2,7};
 		
 		System.out.println("arr values: ");
 		for(int i=0; i<arr.length; i++) {
 			System.out.println(arr[i]);
 		}
 		
-		//passamos, por referentcia, o array, primeira posição e a última posição do array
 		quickSort(arr, 0, arr.length-1);
 		
 		System.out.println("sorted values: ");
 		for(int i=0; i<arr.length; i++) {
 			System.out.println(arr[i]);
 		}
-		
-		System.out.println("Hello World!");
 	}
 	
-	private static void quickSort(int[] arr, int low, int high) {
-		//valida valores
-		if(low < high) {
-			// encontra o índice do pivô após o particionamento
-            int pi = partition(arr, low, high);
+	private static void quickSort(int[] arr, int menor, int maior) {
+		if(menor < maior) {
+			int pivo = partition(arr, menor, maior);
 			
-			// Ordena recursivamente as sublistas
-            quickSort(arr, low, pi - 1);
-            quickSort(arr, pi + 1, high);
+			quickSort(arr, menor, pivo);
+			quickSort(arr, pivo+1, maior);
 		}
 	}
 	
-	private static int partition(int[] arr, int low, int high) {
-		// escolhe o último elemento como pivô
-        int pivot = arr[high];
+	private static int partition(int[] arr, int menor, int maior) {
+		int mid = (menor+maior) / 2;
+		int pivo = arr[mid];
+		int i = menor-1;
+		int j = maior+1;
 
-        // indice do elemento menor que o pivô
-        int i = (low - 1);
-		
-		for(int j=low; j < high; j++) {
-			//valida elemento atual menor q o pivo
-			if(arr[j] <= pivot) {
-				i++;
-				
-				int temp = arr[i];
-				arr[i] = arr[j];
-				arr[j] = temp;
-			}
+		while(true) {
+			do{i++;}while(arr[i]<pivo);
+			do{j--;}while(arr[j]>pivo);
+			
+			if(i>=j) return j;
+			
+			int temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
 		}
-		
-		//troca o pivot com o elemnto i+1
-		int temp = arr[i+1];
-		arr[i+1] = arr[high];
-		arr[high] = temp;
-		
-		//retorna idx do pivo
-		return i + 1;
 	}
 }
